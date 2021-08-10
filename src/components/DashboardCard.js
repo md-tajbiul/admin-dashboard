d92a6055c24963c0071b4ceba8e00a56f5d3a101
Tajbiul <contact.tajbiul@gmail.com>
2021-08-10 22:28:56 +0600

    new style added

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -1,11 +1,26 @@
 import styles from '../../styles/dashboardCard.module.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/styles';
+// import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        borderRadius: "24px",
+        boxShadow: "none",
+        margin: "5px 12px",
+        height: "136px",
+        width: "396px",
+        display: "flex",
+        alignItems: "center",
+    }
+}));
 
 const CustomCard = ({icon, title, amount, color, iconBg}) => {
+    const classes = useStyles();
     return (
         <div style={{paddingTop: '15px'}}>
-            <Card className={styles.card}>
+            <Card className={classes.root}>
             <CardContent>
                 <div className={styles.cardBox}>
                     <div className={styles.cardIcon} style={{backgroundColor: iconBg}}>
