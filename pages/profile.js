59071d5863958b1c3240016a3ee8e680fbcd9561
Tajbiul <contact.tajbiul@gmail.com>
2021-08-10 15:02:52 +0600

    updated build

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,6 @@
 const Profile = () => {
     return (
-        <p style={{fontWeight: '400', style: 'normal', fontSize: '20px', lineHeight: '28px'}}>This is Profile page dummy content.</p>
+        <p style={{fontWeight: '400', style: 'normal', fontSize: '20px', lineHeight: '28px'}}>This is Profile page dummy content. Thanks</p>
     )
 }
 
